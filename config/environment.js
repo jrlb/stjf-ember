f13d96d9c620c88510f0939cd87583109e9c7c07
Robert Boone <robert@rlb3.com>
2015-09-05 11:15:59 -0500

    Add firebase

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'stjf-ember',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://stjf.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
